961b0b3bb514c1dd65e507fc467dfe36341114a9
JungSeungGeun <wjd15sheep@gmail.com>
2024-02-12 02:35:55 +0900

    7과 3번까지

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev +1)
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once")
-  }, []);
-  useEffect(()=> {
-    console.log("I run when 'keyword' changes.")
-  }, [keyword]);
-  useEffect(()=> {
-    console.log("I run when 'counter' changes.")
-  }, [counter]);
-  useEffect(()=> {
-    console.log("I run when keyword & counter changes.")
-  }, [keyword, counter]);
-  return (
-    <div>
-      <input value={keyword}
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+ 
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo("");
+  };
+return (
+  <div>
+    <h1>My to Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+      <button>Add to Do</button>
+    </form>
+    <hr />
+    <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+  </div>
+)
 }
 
 export default App;
