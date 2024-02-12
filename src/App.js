38a3676ddd1c6111efbf6764b665fb0fcd6c195a
JungSeungGeun <wjd15sheep@gmail.com>
2024-02-12 13:03:33 +0900

    Feat: add all

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import { useState } from "react";
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value)
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
- 
-    setToDos(currentArray => [toDo, ...currentArray])
-    setToDo("");
-  };
-return (
-  <div>
-    <h1>My to Dos ({toDos.length})</h1>
-    <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
-      <button>Add to Do</button>
-    </form>
-    <hr />
-    <ul>
-      {toDos.map((item, index) => <li key={index}>{item}</li>)}
-    </ul>
-  </div>
-)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
